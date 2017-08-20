2bcbd3b31dd1d761f85971ca1f9406b5c8cd7f6b
Nismit <nismit.dev@gmail.com>
2017-08-19 17:46:50 -0700

    Modify set reducer name

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 import { authReducer } from './auth/index';
 // import { something } from './folder';
 
+// Can be named Reducer whatever you want
+// Name: reducer
 const rootReducer = combineReducers({
-  authReducer
+  auth: authReducer
 });
 
 export default rootReducer;
