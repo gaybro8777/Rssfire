2e2ea2ef5bc9c9ab433c9752ea43897d98e0969c
Nismit <nismit.dev@gmail.com>
2017-08-20 18:02:49 -0700

    Add auth type

--- src/auth/auth-type.js
+++ src/auth/auth-type.js
@@ -2,3 +2,8 @@ import { createActionType } from '../utils/index';
 
 export const USER_TEST = createActionType('USER_TEST');
 export const USER_SIGNIN_EMAIL = createActionType('USER_SIGNIN_EMAIL');
+
+export const USER_SIGNUP_FACEBOOK = createActionType('USER_SIGNUP_FACEBOOK');
+export const USER_SIGNUP_TWITTER = createActionType('USER_SIGNUP_TWITTER');
+export const USER_SIGNUP_GOOGLE = createActionType('USER_SIGNUP_GOOGLE');
+export const USER_SIGNUP_EMAIL = createActionType('USER_SIGNUP_EMAIL');
