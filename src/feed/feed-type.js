7e7753286926787333794b302c7b99f719b7a30e
Nismit <nismit.dev@gmail.com>
2017-08-22 16:25:45 -0700

    Add divid feeds and category from data which gets from database

--- src/feed/feed-type.js
+++ src/feed/feed-type.js
@@ -1,6 +1,7 @@
 import { createActionType } from '../utils/index';
 
 export const SYSTEM_GET_DATABASE = createActionType('SYSTEM_GET_DATABASE');
+export const SYSTEM_SET_FEEDS_CATEGORIES = createActionType('SYSTEM_SET_FEEDS_CATEGORIES');
 export const SYSTEM_GET_FEEDS = createActionType('SYSTEM_GET_FEEDS');
 export const USER_PULL_REFRESH = createActionType('USER_PULL_REFRESH');
 export const USER_TOUCH_FEED_ITEM = createActionType('USER_TOUCH_FEED_ITEM');
