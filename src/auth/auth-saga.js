577467e864bc6db1982378e2eb06d1cb7ed7478b
Nismit <nismit.dev@gmail.com>
2017-08-21 20:08:09 -0700

    Implement logout and login with email

--- src/auth/auth-saga.js
+++ src/auth/auth-saga.js
@@ -17,7 +17,12 @@
 import { delay } from 'redux-saga';
 import { call, put, take, takeEvery, all } from 'redux-saga/effects';
 import firebase from '../config/firebase';
-import { USER_SIGNUP_GOOGLE, USER_SIGNUP_EMAIL } from './auth-type';
+import {
+  USER_SIGNUP_GOOGLE,
+  USER_SIGNUP_EMAIL,
+  USER_LOGIN_EMAIL,
+  USER_LOGOUT
+} from './auth-type';
 
 firebase.init();
 
@@ -39,6 +44,25 @@ function* signUpWithEmail(action) {
   }
 }
 
+function* loginWithEmail(action) {
+  try {
+    const payload = yield loginWithEmailExec(action);
+    yield put({ type: USER_LOGIN_EMAIL.SUCCESS, payload });
+  } catch(error) {
+    yield put({ type: USER_LOGIN_EMAIL.ERROR, error: error.message });
+  }
+}
+
+function* logout(action) {
+  console.log('Saga: sign out');
+  try {
+    yield logoutExec();
+    yield put({ type: USER_LOGOUT.SUCCESS });
+  } catch(error) {
+    yield put({ type: USER_LOGOUT.ERROR, error: error.message });
+  }
+}
+
 // Exec
 function* signUpWithEmailExec(action) {
   const param = [action.email, action.password];
@@ -49,7 +73,18 @@ function* signUpWithGoogleExec() {
   return yield call(firebase.signInWithGooglePopup);
 }
 
+function* loginWithEmailExec(action) {
+  const param = [action.email, action.password];
+  return yield call(firebase.loginWithEmail, ...param);
+}
+
+function* logoutExec() {
+  return yield call(firebase.logout);
+}
+
 export const authSaga = [
   takeEvery(USER_SIGNUP_GOOGLE.PENDING, signUpWithGoogle),
-  takeEvery(USER_SIGNUP_EMAIL.PENDING, signUpWithEmail)
+  takeEvery(USER_SIGNUP_EMAIL.PENDING, signUpWithEmail),
+  takeEvery(USER_LOGIN_EMAIL.PENDING, loginWithEmail),
+  takeEvery(USER_LOGOUT.PENDING, logout)
 ];
