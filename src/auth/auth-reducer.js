a175e15cff14af0a36d178617c33c5cd73eac8f6
Nismit <nismit.dev@gmail.com>
2017-08-21 17:13:09 -0700

    Add google sign in state

--- src/auth/auth-reducer.js
+++ src/auth/auth-reducer.js
@@ -1,7 +1,7 @@
 // return state
 
 // import type
-import { USER_TEST, USER_SIGNIN_EMAIL, USER_SIGNUP_EMAIL } from './auth-type';
+import { USER_SIGNUP_GOOGLE, USER_SIGNUP_EMAIL } from './auth-type';
 
 const initialState = {
   user: {},
@@ -12,6 +12,23 @@ const initialState = {
 export const authReducer = (state = initialState, action = {}) => {
   console.log('Exec auth Reducer:' + action.type);
   switch(action.type) {
+    case USER_SIGNUP_GOOGLE.PENDING:
+      return {
+        ...state,
+        isPendingSignUp: true,
+      };
+    case USER_SIGNUP_GOOGLE.SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        isPendingSignUp: false,
+      };
+    case USER_SIGNUP_GOOGLE.ERROR:
+      return {
+        ...state,
+        isPendingSignUp: false,
+        error: action.error,
+      };
     case USER_SIGNUP_EMAIL.PENDING:
       return {
         ...state,
