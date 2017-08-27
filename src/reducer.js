9cc5d07f8419f040d8657159e922fe37c68b5d1a
Nismit <nismit.dev@gmail.com>
2017-08-26 18:10:33 -0700

    Add subscribe reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 
 import { authReducer } from './auth/index';
 import { feedReducer } from './feed/index';
+import { subscribeReducer } from './subscribe/index';
 
 // Can be named Reducer whatever you want
 // Name: reducer
 const rootReducer = combineReducers({
   auth: authReducer,
-  feed: feedReducer
+  feed: feedReducer,
+  subscribe: subscribeReducer,
 });
 
 export default rootReducer;
