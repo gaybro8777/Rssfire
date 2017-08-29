a32dd688f310b009501667a80f9e040f34fc7fa5
Nismit <nismit.dev@gmail.com>
2017-08-29 02:02:58 -0700

    Add style

--- src/components/feed-item.js
+++ src/components/feed-item.js
@@ -2,19 +2,39 @@ import React from 'react';
 import { StyleSheet, Text, Image, View } from 'react-native';
 
 const styles = StyleSheet.create({
-  fullViewContainer: {
+  itemContainer: {
     flex: 1,
-    // flexDirection: 'column',
     flexDirection: 'row',
-    // justifyContent: 'flex-start',
-    justifyContent: 'center',
-    // alignItems: 'stretch',
-    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+  },
+  imageContainer: {
+    width: 60,
+    height: 60,
+    marginRight: 10,
+  },
+  textContainer: {
+    width: 0,
+    flexGrow: 1,
+    flex: 1,
+  },
+  textTitle : {
+    fontWeight: 'bold',
+  },
+  textSite : {
+    fontSize: 12,
   },
 });
 
-export const FeedItem = ({index, title, desc}) =>
-  <View key={index}>
-    <Text>{index}</Text>
-    <Text>{title}</Text>
+export const FeedItem = ({index, title, desc, site}) =>
+  <View key={index} style={styles.itemContainer}>
+    <Image
+      style={styles.imageContainer}
+      source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+    />
+    <View style={styles.textContainer}>
+      <Text style={styles.textTitle}>{title}</Text>
+      <Text style={styles.textSite} numberOfLines={1}>{site}</Text>
+    </View>
   </View>;
