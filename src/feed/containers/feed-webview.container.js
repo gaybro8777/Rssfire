938422f39dabde397a67d7d572f3aaa3cbf6a1c7
Nismit <nismit.dev@gmail.com>
2017-08-31 16:57:03 -0700

    Modify feed webview container

--- src/feed/containers/feed-webview.container.js
+++ src/feed/containers/feed-webview.container.js
@@ -4,26 +4,4 @@ import { connect } from 'react-redux';
 // import view from screen directory
 import { FeedWebViewScreen } from '../screen/feed-webview.screen';
 
-import {
-  SYSTEM_GET_DATABASE,
-  SYSTEM_GET_FEEDS,
-  USER_PULL_REFRESH,
-  USER_TOUCH_FEED_ITEM
-} from '../feed-type';
-
-// set state from reducer
-const mapStateToProps = state => ({
-  url: state.feed.url,
-  error: '',
-});
-
-// set dispatch to saga
-const mapDispatchToProps = dispatch => ({
-  getDatabaseByDispatch: () => {
-    dispatch({
-      type: SYSTEM_GET_DATABASE.PENDING
-    })
-  },
-});
-
-export const FeedWebviewContainer = connect(mapStateToProps, mapDispatchToProps)(FeedWebViewScreen);
+export const FeedWebviewContainer = connect(null, null)(FeedWebViewScreen);
