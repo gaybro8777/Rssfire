a0f67abbe95c451a15ca599faaf88e048e219f26
Nismit <nismit.dev@gmail.com>
2017-08-31 00:43:17 -0700

    Add bottom control view and style

--- src/feed/screen/feed-webview.screen.js
+++ src/feed/screen/feed-webview.screen.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
+import { WebView, StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'react-native-elements';
+
 import { LoadingIndicator } from '../../components/index';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  webViewContainer: {
+    width: '100%',
+    height: '95%',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  iconBase: {
+    padding: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  refreshIcon: {
+    position: 'relative',
+    top: 1,
+  },
+});
 
 class FeedWebView extends Component {
   constructor(props) {
@@ -19,11 +46,39 @@ class FeedWebView extends Component {
     } = this.props.navigation.state.params.item;
 
     return (
-      <WebView
-        source={{ uri: link }}
-        renderLoading={() => <LoadingIndicator />}
-        startInLoadingState
-      />
+      <View style={styles.wrapper}>
+        <WebView
+          source={{ uri: link }}
+          renderLoading={() => <LoadingIndicator />}
+          startInLoadingState
+        />
+        <View style={styles.bottomContainer}>
+          <Icon
+            name='ios-arrow-back'
+            type='ionicon'
+            size={30}
+            iconStyle={styles.iconBase}
+            onPress={() => console.log('test') } />
+          <Icon
+            name='ios-arrow-forward'
+            type='ionicon'
+            size={30}
+            iconStyle={styles.iconBase}
+            onPress={() => console.log('test') } />
+          <Icon
+            name='ios-refresh'
+            type='ionicon'
+            size={33}
+            iconStyle={[styles.iconBase, styles.refreshIcon]}
+            onPress={() => console.log('test') } />
+          <Icon
+            name='share-apple'
+            type='evilicon'
+            size={32}
+            iconStyle={styles.iconBase}
+            onPress={() => console.log('test') } />
+        </View>
+      </View>
     );
   }
 }
