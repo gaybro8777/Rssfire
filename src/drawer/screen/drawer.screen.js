8ded13fbe99203ceca6c1ccdcaaf16c5b0f406d3
Nismit <nismit.dev@gmail.com>
2017-09-01 16:56:28 -0700

    WIP add List in drawer

--- src/drawer/screen/drawer.screen.js
+++ src/drawer/screen/drawer.screen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 
 class Drawer extends Component {
   constructor(props) {
@@ -11,26 +12,43 @@ class Drawer extends Component {
     snapshot: Object,
   };
 
-  componentWillMount() {
-  }
-
-  componentDidMount() {
-  }
-
   render() {
     const {
       navigation,
       snapshot,
     } = this.props;
 
-    console.log('Drawer Props:' + JSON.stringify(this.props));
+    console.log(snapshot);
+
+    if (Object.keys(snapshot).length === 0) { console.log('length 0') }
+
+    // const hasFeeds = () => {
+    //   if(snapshot
+    // }
 
     return (
       <View>
-        <Text>Test</Text>
+        <Text>Feed List</Text>
+        <List>
+          <ListItem
+            key="key-logout"
+            title="Logout"
+          />
+        </List>
       </View>
     );
   }
 }
+
+// <List>
+// {
+//   list.map((l, i) => (
+//     <ListItem
+//       key={i}
+//       title={l.name}
+//     />
+//   ))
+// }
+// </List>
 
 export const DrawerScreen = Drawer;
