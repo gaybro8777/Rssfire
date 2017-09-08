e1d6ce2859e34d49946d7effec6856c0d799f5d8
Nismit <nismit.dev@gmail.com>
2017-09-07 18:50:30 -0700

    Add dispatch

--- src/drawer/containers/drawer.container.js
+++ src/drawer/containers/drawer.container.js
@@ -4,9 +4,22 @@ import { connect } from 'react-redux';
 // import view from screen directory
 import { DrawerScreen } from '../screen/drawer.screen';
 
+import { USER_TOUCH_LOGOUT } from '../drawer-type';
+
 // set state from reducer
 const mapStateToProps = state => ({
   snapshot: state.feed.snapshot,
+  isPendingLogout: state.drawer.isPendingLogout,
 });
 
-export const DrawerContainer = connect(mapStateToProps, null)(DrawerScreen);
+// set dispatch to saga
+const mapDispatchToProps = dispatch => ({
+  tryLogoutByDispatch: navigation => {
+    dispatch({
+      type: USER_TOUCH_LOGOUT.PENDING,
+      navigation
+    })
+  },
+});
+
+export const DrawerContainer = connect(mapStateToProps, mapDispatchToProps)(DrawerScreen);
