585769f5e721a89e4b051cc4e4f8884cadcc92f1
Nismit <nismit.dev@gmail.com>
2017-09-11 00:22:56 -0700

    Add type

--- src/feed/feed-type.js
+++ src/feed/feed-type.js
@@ -2,5 +2,6 @@ import { createActionType } from '../utils/index';
 
 export const SYSTEM_GET_SNAPSHOT = createActionType('SYSTEM_GET_SNAPSHOT');
 export const SYSTEM_GET_FEEDS = createActionType('SYSTEM_GET_FEEDS');
+export const SYSTEM_FILTER_FEEDS = createActionType('SYSTEM_FILTER_FEEDS');
 export const USER_PULL_REFRESH = createActionType('USER_PULL_REFRESH');
 export const USER_TOUCH_FEED_ITEM = createActionType('USER_TOUCH_FEED_ITEM');
