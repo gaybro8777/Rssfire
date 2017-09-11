22a3de607d5ac555aaf4f7c127d4d06e3f8f2e01
Nismit <nismit.dev@gmail.com>
2017-09-11 00:19:24 -0700

    Add type

--- src/drawer/drawer-type.js
+++ src/drawer/drawer-type.js
@@ -1,3 +1,4 @@
 import { createActionType } from '../utils/index';
 
 export const USER_TOUCH_LOGOUT = createActionType('USER_TOUCH_LOGOUT');
+export const USER_TOUCH_FEED = createActionType('USER_TOUCH_FEED');
