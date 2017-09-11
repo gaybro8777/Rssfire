4719897196f074f117d689919b67e21409ecf788
Nismit <nismit.dev@gmail.com>
2017-09-11 00:22:20 -0700

    Implement to call feed saga

--- src/drawer/drawer-saga.js
+++ src/drawer/drawer-saga.js
@@ -1,8 +1,9 @@
 import { AsyncStorage } from 'react-native';
-import { call, put, take, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 import firebase from '../config/firebase';
 import { resetNavigation } from '../utils/index';
-import { USER_TOUCH_LOGOUT } from './drawer-type';
+import { USER_TOUCH_LOGOUT, USER_TOUCH_FEED } from './drawer-type';
+import { SYSTEM_FILTER_FEEDS } from '../feed/index';
 
 function* tryLogout(action) {
   try {
@@ -12,6 +13,16 @@ function* tryLogout(action) {
     resetNavigation('Splash', action.navigation);
   } catch(error) {
     yield put({ type: USER_TOUCH_LOGOUT.ERROR, error: error.message });
+  }
+}
+
+function* callFilterFeed(action) {
+  try {
+    yield put({ type: SYSTEM_FILTER_FEEDS.PENDING, filter: action.url });
+    yield put({ type: USER_TOUCH_FEED.SUCCESS });
+    resetNavigation('Home', action.navigation);
+  } catch(error) {
+    yield put({ type: USER_TOUCH_FEED.ERROR, error: error.message });
   }
 }
 
@@ -25,5 +36,6 @@ function* removeUIDExec() {
 }
 
 export const drawerSaga = [
-  takeEvery(USER_TOUCH_LOGOUT.PENDING, tryLogout)
+  takeEvery(USER_TOUCH_LOGOUT.PENDING, tryLogout),
+  takeEvery(USER_TOUCH_FEED.PENDING, callFilterFeed)
 ];
