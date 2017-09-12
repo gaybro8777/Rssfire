406b6696d2b49f279e1e3268a1258483a0357fa3
Nismit <nismit.dev@gmail.com>
2017-09-12 15:01:17 -0700

    Add bugsnag when prod mode is working

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,10 @@
 */
 
 import { AppRegistry } from 'react-native';
+import { Client } from 'bugsnag-react-native';
 import App from './src/App';
 
+if(__DEV__ !== true) {
+  const bugsnag = new Client();
+}
 AppRegistry.registerComponent('RssFire', () => App);
