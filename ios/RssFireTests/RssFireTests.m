df6c7ec4babb8172e19263d6bec993cb56e8d20f
Nismit <nismit.dev@gmail.com>
2017-09-11 22:32:13 -0700

    Changes project name

--- ios/RssFireTests/RssFireTests.m
+++ ios/RssFireTests/RssFireTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface SimpleRSSReaderTests : XCTestCase
+@interface RssFireTests : XCTestCase
 
 @end
 
-@implementation SimpleRSSReaderTests
+@implementation RssFireTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
